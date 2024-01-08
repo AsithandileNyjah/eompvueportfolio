8797da3846dabd5afe5f4e63a3c5c46a88c41874
AsithandileNyjah <atyuluu@gmail.com>
2024-01-08 11:50:28 +0000

    My stuff

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <router-link class="nav-link active" to="/" aria-current="page">Home</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link class="nav-link" to="/about">About</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link class="nav-link" to="/resume">Resume</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link class="nav-link" to="/projects">Projects</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link class="nav-link" to="testimonials">Testimonials</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link class="nav-link" to="/contact">Contact</router-link>
+  </li>
+</ul>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div class="containter-fluid">
+    <p>This is the about page</p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
